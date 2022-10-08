a26e579307a2f18e0a6c6f261be498793c9ba4a8
ddbmajor <nikita.vasilev.200279@gmail.com>
2022-10-08 11:23:28 +0300

    added values

--- lab_02/sql/23.sql
+++ lab_02/sql/23.sql
@@ -11,7 +11,9 @@ CREATE TABLE public.employee (
 INSERT INTO public.employee
 VALUES 
     (1, 'Никита', 'Петров', 'Проджект менеджер',16, NULL),
-    (2, 'Василий', 'Сидоров', 'Разработчик', 16, 1);
+    (2, 'Василий', 'Сидоров', 'Разработчик', 16, 1),
+    (3, 'Давид', 'Щепкин', 'Стажер', 16, 2),
+    (4, 'Серафим', 'Пупкин', 'Стажер', 16, 2);
 -- Определение ОТВ
 WITH RECURSIVE DirectReports (ManagerID, id, title, DeptID, Level) AS
 (
